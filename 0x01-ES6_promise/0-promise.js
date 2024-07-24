4ea300c2b7e2e8120d49b48351060374f8482fb8
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-25 00:03:01 +0100

    Updated code

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,17 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve) => resolve);
+// export default function getResponseFromAPI() {
+//     return new Promise((resolve) => resolve);
+//   }
+// 0-promise.js
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const responseData = {
+          message: 'Hello from API!',
+          data: [1, 2, 3]
+        };
+        resolve(responseData);
+      }, 2000);
+    });
   }
+  
+  export default getResponseFromAPI;
