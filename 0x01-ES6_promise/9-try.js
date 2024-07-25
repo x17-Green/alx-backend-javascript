df65facfe4285a618db9b6ea4349d4c207cd9b43
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-25 13:55:31 +0100

    Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(`${error.name}: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
