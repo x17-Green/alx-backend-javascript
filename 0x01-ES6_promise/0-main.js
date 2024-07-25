a5ab05af6a21dca330382caa9153ece25399d939
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-25 08:32:38 +0100

    Reconfigured package for requirements

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -1,4 +1,3 @@
-
 import getResponseFromAPI from "./0-promise.js";
 
 const response = getResponseFromAPI();
--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,13 +2,29 @@
 
 function getFullResponseFromAPI(success) {
     if (success) {
-        return Promise.resolve({
+        return new Promise.resolve({
             status: 200,
             body: 'Success'
         });
     } else {
-        return Promise.reject(new Error('The fake API is not working currently'));
+        return new Promise.reject(Error('The fake API is not working currently'));
     }
 }
 
 export default getFullResponseFromAPI;
+
+
+// Second method
+
+// export default function getFullResponseFromAPI(success) {
+//     return new Promise((resolve, reject) => {
+//       if (success) {
+//         resolve({
+//           status: 200,
+//           body: 'Success',
+//         });
+//       } else {
+//         reject(Error('The fake API is not working currently'));
+//       }
+//     });
+//   }
