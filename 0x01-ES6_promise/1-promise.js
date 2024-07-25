be8ae2657970c33912faffe6a63b256ac675809d
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-25 08:45:05 +0100

    Updated code for task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,25 +1,25 @@
-export default function getFullResponseFromAPI(success) {
-    if (success) {
-        return Promise.resolve({
-            status: 200,
-            body: 'Success'
-        });
-    } else {
-        return Promise.reject(Error('The fake API is not working currently'));
-    }
-}
+// export default function getFullResponseFromAPI(success) {
+//     if (success) {
+//         return Promise.resolve({
+//             status: 200,
+//             body: 'Success'
+//         });
+//     } else {
+//         return Promise.reject(Error('The fake API is not working currently'));
+//     }
+// }
 
 // Second method
 
-// export default function getFullResponseFromAPI(success) {
-//     return new Promise((resolve, reject) => {
-//       if (success) {
-//         resolve({
-//           status: 200,
-//           body: 'Success',
-//         });
-//       } else {
-//         reject(Error('The fake API is not working currently'));
-//       }
-//     });
-//   }
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({
+                status: 200,
+                body: 'Success',
+            });
+        } else {
+            reject(Error('The fake API is not working currently'));
+        }
+    });
+}
