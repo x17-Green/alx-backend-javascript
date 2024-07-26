3863daece88c233e87d6e3100a88d0afacfbc1ab
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-26 06:54:57 +0100

    ES6 method properties: Rewrite a funtion to use ES6 method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,16 +1,27 @@
+// import getBudgetObject from './7-getBudgetObject';
+
+// export default function getFullBudgetObject(income, gdp, capita) {
+//   const budget = getBudgetObject(income, gdp, capita);
+//   const fullBudget = {
+//     ...budget,
+//     getIncomeInDollars() {
+//       return `$${income}`;
+//     },
+//     getIncomeInEuros() {
+//       return `${income} euros`;
+//     },
+//   };
+
+//   return fullBudget;
+// }
+
+
 import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars() {
-      return `$${income}`;
-    },
-    getIncomeInEuros() {
-      return `${income} euros`;
-    },
-  };
-
-  return fullBudget;
+  return Object.assign(budget, {
+    getIncomeInDollars: () => `$${budget.income}`,
+    getIncomeInEuros: () => `${budget.income} euros`,
+  });
 }
