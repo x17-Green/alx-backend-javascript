9660fc37dc7bd38170561a275fa3f111929094c4
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-27 18:16:14 +0100

    Make a classroom: Implement a function that returns an array of 3 ClassRoom objects

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+// 1-make_classrooms.js
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+}
+
+export default initializeRooms;
