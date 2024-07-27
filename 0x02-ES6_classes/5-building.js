35084c3738e05e8a3ac9e3acd378891fb90a783a
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-27 20:17:23 +0100

    A building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+// 5-building.js
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+}
