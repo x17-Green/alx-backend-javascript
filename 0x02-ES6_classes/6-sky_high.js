0d5eca2746d0d85806001c9d4a0551ef7103e398
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-27 22:52:48 +0100

    Inheritance: Extend from a class and over the method to return a string

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,32 @@
+// Import the Building class from 5-building.js
+import Building from './5-building.js';
+
+// Implement a class named SkyHighBuilding that extends from Building
+class SkyHighBuilding extends Building {
+  // Constructor attributes
+  constructor(sqft, floors) {
+    // Call the parent class constructor with sqft
+    super(sqft);
+    // Assign floors to the _floors attribute
+    this._floors = floors;
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    // Return the evacuation warning message with the number of floors
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
+
+// Export the SkyHighBuilding class
+export default SkyHighBuilding;
