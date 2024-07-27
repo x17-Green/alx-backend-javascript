a13a4a0e86aef848337168b13bc15cc2aa28dffb
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-27 22:42:05 +0100

    Updated code for:  5 - A building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,21 +2,21 @@
 
 // Implement a class named Building
 class Building {
-    // Constructor attribute
-    constructor(sqft) {
-      // Assign sqft to the _sqft attribute
-      this._sqft = sqft;
-      // Check if the subclass has implemented the evacuationWarningMessage method
-      if (this.constructor !== Building && !this.constructor.prototype.evacuationWarningMessage) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-    }
-  
-    // Getter for sqft
-    get sqft() {
-      return this._sqft;
+  // Constructor attribute
+  constructor(sqft) {
+    // Assign sqft to the _sqft attribute
+    this._sqft = sqft;
+    // Check if the subclass has implemented the evacuationWarningMessage method
+    if (this.constructor !== Building && !this.constructor.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
-  
-  // Export the Building class
-  export default Building;
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+}
+
+// Export the Building class
+export default Building;
