cec1c78c8a8198b4b97de71db31f67caee726df3
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-29 21:14:36 +0100

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,29 @@
+// 1-get_list_student_ids.js
+
+/**
+ * Returns a list of student IDs from an array of student objects.
+ *
+ * @param {Array<Object>} students
+ * - An array of student objects, each with an 'id' property.
+ * @returns {Array<number>}
+ * - An array of student IDs.
+ * - If the input is not an array, an empty array is returned.
+ *
+ * @example
+ * const students = [
+ *   { id: 1, name: 'John Doe' },
+ *   { id: 2, name: 'Jane Doe' },
+ *   { id: 3, name: 'Bob Smith' },
+ * ];
+ * const studentIds = getListStudentIds(students);
+ * console.log(studentIds); // [1, 2, 3]
+ */
+
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return new TypeError(Error, "Must be a student function");
+  }
+  return students.map((student) => student.id);
+}
+
+export default getListStudentIds;
