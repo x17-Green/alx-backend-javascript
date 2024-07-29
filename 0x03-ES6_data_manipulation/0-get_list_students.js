a1010ef3b067c2ebad10b1d56c81641568a9ddb8
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-29 17:48:52 +0100

    Created a function that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,41 @@
+// 0-get_list_students.js
+
+/**
+ * Returns a list of student objects.
+ *
+ * @returns {Array<Object>} A list of student objects,
+ * each containing id, firstName, and location properties.
+ *
+ * @example
+ * const students = getListStudents();
+ * console.log(students);
+ * // Output:
+ * // [
+ * //   { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+ * //   { id: 2, firstName: 'James', location: 'Columbia' },
+ * //   { id: 3, firstName: 'Guillaume', location: 'San Francisco' },
+ * // ]
+ */
+function getListStudents() {
+  const students = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 3,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+  ];
+
+  return students;
+}
+
+console.log(getListStudents());
