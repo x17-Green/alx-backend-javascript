2b0d96e03d21912d3f00d179491d19d05da219d2
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-30 17:27:31 +0100

    (Updated docstrings)

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,28 +1,28 @@
 // 8-clean_set.js
 
 /**
- * Cleans a set of strings by removing a specified start
- * string and joining the remaining values with a hyphen.
+ * Cleans a set of strings by filtering out strings that start with a specified start string
+ * and then removing the start string from the remaining strings.
  *
  * @param {Set<string>} set - The set of strings to clean.
- * @param {string} startString - The string to remove from
- * the start of each value in the set.
- * @returns {string} The cleaned and joined string values.
+ * @param {string} startString - The string that the strings in the set should start with.
+ * @returns {string} A string containing the cleaned strings joined by a hyphen.
+ *
  * @example
- * const mySet = new Set(['hello/world', 'hello/foo', 'bar/baz']);
- * const cleaned = cleanSet(mySet, 'hello/');
- * console.log(cleaned); // Output: "world-foo"
+ * const mySet = new Set(['hello world', 'hello universe', 'foo bar', 'hello earth']);
+ * const cleanedString = cleanSet(mySet, 'hello ');
+ * console.log(cleanedString); // Output: "world-universe-earth"
  */
 function cleanSet(set, startString) {
-  if (startString === '' || typeof startString !== 'string') return '';
-
-  const filteredStrings = [];
-  set.forEach((s) => {
-    if (typeof s === 'string' && s.startsWith(startString)) {
-      filteredStrings.push(s.slice(startString.length));
-    }
-  });
-  return filteredStrings.join('-');
-}
-
-export default cleanSet;
+    if (startString === '' || typeof startString !== 'string') return '';
+  
+    const filteredStrings = [];
+    set.forEach((s) => {
+      if (typeof s === 'string' && s.startsWith(startString)) {
+        filteredStrings.push(s.slice(startString.length));
+      }
+    });
+    return filteredStrings.join('-');
+  }
+  
+  export default cleanSet;
