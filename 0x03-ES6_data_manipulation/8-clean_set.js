8936c696f8410f519454b64d17c34f1c37393d68
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-30 15:09:34 +0100

    (Updated code) Clean set: A function that returns strings of set of values that start with certain word

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,21 @@
 // 8-clean_set.js
 
 /**
- * Cleans a set of strings by removing a specified start
- * string and returns the cleaned set as a hyphen-separated string.
+ * Cleans a set of strings by removing a specified start string and joining the remaining values with a hyphen.
  *
- * @param {Set} set - The set of strings to be cleaned.
- * @param {string} startString - The string to be removed from \
- * the start of each string in the set.
- * @returns {string} A hyphen-separated string of the cleaned set.
- *
+ * @param {Set<string>} set - The set of strings to clean.
+ * @param {string} startString - The string to remove from the start of each value in the set.
+ * @returns {string} The cleaned and joined string values.
  * @example
- * const mySet = new Set(['hello-world', 'hello-universe', 'foo-bar']);
- * const cleanedSet = cleanSet(mySet, 'hello-');
- * console.log(cleanedSet); // Output: "world-universe"
+ * const mySet = new Set(['hello/world', 'hello/foo', 'bar/baz']);
+ * const cleaned = cleanSet(mySet, 'hello/');
+ * console.log(cleaned); // Output: "world-foo"
  */
 function cleanSet(set, startString) {
   const filteredValues = [];
+  if (startString == '' || typeof startString !== 'string') {
+    return '';
+  }
   for (const value of set) {
     if (typeof value === 'string' && value.startsWith(startString)) {
       filteredValues.push(value.substring(startString.length));
