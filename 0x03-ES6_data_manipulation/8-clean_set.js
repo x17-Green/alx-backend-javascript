99466f9791a9ac230f4a3abe71d430a7393ffc89
Okoyen E. Precious <x17.ebi@icloud.com>
2024-07-30 15:21:49 +0100

    (Updated code) Clean set: A function that returns strings of set of values that start with certain word

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -11,16 +11,39 @@
  * const cleaned = cleanSet(mySet, 'hello/');
  * console.log(cleaned); // Output: "world-foo"
  */
+// function cleanSet(set, startString) {
+//     if (startString === '' || typeof startString !== 'string') return '';
+  
+//     const filteredStrings = [];
+//     set.forEach((s) => {
+//       if (typeof s === 'string' && s.startsWith(startString)) {
+//         filteredStrings.push(s.slice(startString.length));
+//       }
+//     });
+//     return filteredStrings.join('-');
+//   };
+  
+//   export default cleanSet;
+
+
+/**
+ * Returns a string of all the set values that start with a specific string.
+ * 
+ * @param {Set} set - A set of strings.
+ * @param {String} startString - The string to filter the set values.
+ * @returns {String} A string containing all the filtered set values separated by '-'.
+ */
 function cleanSet(set, startString) {
-    if (startString === '' || typeof startString !== 'string') return '';
-  
-    const filteredStrings = [];
-    set.forEach((s) => {
-      if (typeof s === 'string' && s.startsWith(startString)) {
-        filteredStrings.push(s.slice(startString.length));
-      }
-    });
-    return filteredStrings.join('-');
-  };
-  
-  export default cleanSet;
+    if (startString === '' || typeof startString !== 'string') {
+        return '';
+    }
+    // Filter the set values that start with the startString
+    const filteredValues = [...set].filter(value => value.startsWith(startString));
+
+    // Remove the startString from the filtered values and join them with '-'
+    const cleanedString = filteredValues.map(value => value.slice(startString.length)).join('-');
+
+    return cleanedString;
+}
+
+export default cleanSet;
